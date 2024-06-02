e324a4bf2f7273ca15f7a685e89b4f5e9a4c57ef
jolene <sitienijolene@gmail.com>
2024-06-02 08:23:55 +0300

    write a function that returns the first object in the campsites array

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,7 +4,12 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 }
 
-export const selectRandomCampsite = () => {
-    // wrong tessy return Math.floor(Math.random() * selectAllCampsites.length);
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// export const selectRandomCampsite = () => {
+//     // wrong tessy return Math.floor(Math.random() * selectAllCampsites.length);
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// }
+
+//function takes an argument of id and returns the first object in the CAMPSITES array with a matching id
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 }
