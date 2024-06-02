9292842eca3ae8f40deaebd821d2c16dfb82a0bf
jolene <sitienijolene@gmail.com>
2024-06-02 10:25:18 +0300

    update the Header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,46 @@
 //reactstrap
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem} from 'reactstrap';
 //logo
 import NucampLogo from "../app/assets/img/logo.png";
+import { NavLink } from 'react-router-dom';
+import NucampLogo from '../app/assets/img/logo.png';
 
 //creating the Header component
 const Header = () => {
     return(
         <Navbar dark color = 'primary' sticky= 'top' expand = 'md'>
-                <NavbarBrand href = '/'>
-                    <img src = {NucampLogo} alt ='nucamp logo'/>
+                <NavbarBrand href = '/' className='ms-5'>
+                    <img src = {NucampLogo} alt ='nucamp logo'className='float-start'/>
+                    <h1 className='mt-1'>NuCamp</h1>
                 </NavbarBrand>
+                <Nav className = 'ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className= 'nav-link' to ='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className= 'nav-link' to ='/directory'>
+                        <i className='fa fa-list fa-lg' /> Directory
+                        </NavLink>
+                        
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className= 'nav-link' to ='/about'>
+                            <i className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                        
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className= 'nav-link' to ='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                        
+                    </NavItem>
+                </Nav>
       </Navbar>
+      
     );
 }
 
